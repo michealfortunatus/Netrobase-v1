7141d64b5184eee16bb413b83c98e3f6cdd85614
michealfortunatus <michealfortunatus@gmail.com>
2023-04-14 20:39:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// ========================================navbar-toggle=======================================
+const navopenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar= document.querySelector("[data-navbar]");
+const navCloseBtn= document.querySelector("[data-nav-close-btn]");
+const overlay= document.querySelector("[data-overlay]");
+
+const elemArr = [navCloseBtn, overlay, navopenBtn];
+
+for(let i =0; i < elemArr.length; i++){
+    elemArr[i].addEventListener("click", function() {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+}
+
+//=========================================== navlink toggle============================================
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i=0; i < navbarLinks.length; 1++){
+    navbarLinks[1].addEventListener("click", function() {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+}
+
+
+//========================== header and go -top active when window scroll down to 400px================
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY >= 400){
+        header.classList.add("active");
+        goTopBtn.classList.add("active");
+    }
+        else{
+            header.classList.remove("active");
+            goTopBtn.classList.remove("active");
+        }
+    }
+);
